4315820af3835fecad01594d3d05e230ea532ad7
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2022-12-07 16:39:35 +0800

    add autotest

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using TeneT_demo
 using Test
 
 @testset "TeneT_demo.jl" begin
-    # Write your tests here.
+    include("hamiltonian_models.jl")
+    include("contruct_M.jl")
+    include("observable.jl")
+    include("optimise_ipeps.jl")
 end
